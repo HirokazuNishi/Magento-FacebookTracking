c80b96a572b3ff882ede019a4f389b949b0b5d89
Hirokazu Nishi <nishi@principle-works.jp>
2015-02-19 22:48:32 +0900

    version 1.0.0

---
+++ src/app/code/community/Rack/Fbtracking/Block/Conversion.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@principle-works.jp so we can send you a copy immediately.
+ *
+ * @category   Marketing
+ * @package    Rack_Fbtracking
+ * @copyright  Copyright (c) 2015 Veriteworks Inc. (http://principle-works.jp/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Rack_Fbtracking_Block_Conversion extends Mage_Core_Block_Template
+{
+
+}
